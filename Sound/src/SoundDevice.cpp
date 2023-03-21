1a558e6908e0b679024ca38c4119417f76941f22
Noah Cohn <noahc42@gmail.com>
2023-03-21 11:51:11 -0700

    Update SoundDevice.cpp

--- Sound/src/SoundDevice.cpp
+++ Sound/src/SoundDevice.cpp
@@ -1,31 +1,41 @@
 #include "SoundDevice.h"
 
-SoundDevice::SoundDevice(){
+SoundDevice::SoundDevice()
+{
    // open default device
-m_Device = alcOpenDevice(nullptr);
-if (!m_Device) // verify success
-// create new context
-m_Context=alcCreateContext(m_Device,nullptr);
-if(!m_Context)   // verify success
-// make context current
-if(!alcMakeContextCurrent(m_Context)) // verify success
+   m_Device = alcOpenDevice(nullptr);
+   if (!m_Device) // verify success
+
+   // create new context
+   m_Context = alcCreateContext(m_Device, nullptr);
+   if (!m_Context)   // verify success
+
+   // make context current
+   if (!alcMakeContextCurrent(m_Context)) // verify success
+
    // get name of device opened from enumerate all extension
-const ALCchar* name = nullptr;
-if (alcIsExtensionPresent(m_Device, "ALC_ENUMERATE_ALL_EXT"))
-name = alcGetString(m_Device, ALC_ALL_DEVICES_SPECIFIER);
-else
-if (!name || !alcGetError(m_Device))
-name = alcGetString(m_Device, ALC_DEVICE_SPECIFIER);
-std::cout << "Opened Device: " << name << std::endl;
+   const ALCchar* name = nullptr;
+
+   if (alcIsExtensionPresent(m_Device, "ALC_ENUMERATE_ALL_EXT"))
+      name = alcGetString(m_Device, ALC_ALL_DEVICES_SPECIFIER);
+   else
+
+   if (!name || !alcGetError(m_Device))
+      name = alcGetString(m_Device, ALC_DEVICE_SPECIFIER);
+
+   std::cout << "Opened Device: " << name << std::endl;
 }
 
-SoundDevice::~SoundDevice(){
+SoundDevice::~SoundDevice()
+{
    // clear current context
-if (!alcMakeContextCurrent(nullptr))
-DisplayALCError("Error Unbinding Current Context", alcGetError(m_Device));
+   if (!alcMakeContextCurrent(nullptr))
+   DisplayALCError("Error Unbinding Current Context", alcGetError(m_Device));
+
    // destroy context
-alcDestroyContext(m_Context);
-DisplayALCError("Error Destroying Context", alcGetError(m_Device));
+   alcDestroyContext(m_Context);
+   DisplayALCError("Error Destroying Context", alcGetError(m_Device));
+
    // close active device
-if (!alcCloseDevice(m_Device))
+   if (!alcCloseDevice(m_Device))
 }
