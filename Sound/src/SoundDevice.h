33ab06d9dbe91e5d0104bb0b1309212c641ba59f
Noah Cohn <noahc42@gmail.com>
2023-03-21 09:55:28 -0700

    Update SoundDevice.h

--- Sound/src/SoundDevice.h
+++ Sound/src/SoundDevice.h
@@ -5,20 +5,15 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 
-void DisplayALError(const char* msg, ALenum error);
-void DisplayALCError(const char* msg, ALCenum error);
+void DisplayALError(const char* msg,ALenum error);
+void DisplayALCError(const char* msg,ALCenum error);
 
-//! OpenAL Sound Device
-// Holds current OpenAL context and devices
-// Should be destroyed LAST
-class SoundDevice
-{
+class SoundDevice{
 public:
-	SoundDevice();
-	~SoundDevice();
+SoundDevice();
+~SoundDevice();
 
 private:
-	ALCdevice* m_Device = nullptr;
-	ALCcontext* m_Context = nullptr;
-
+ALCdevice* m_Device=nullptr;
+ALCcontext* m_Context=nullptr;
 };
