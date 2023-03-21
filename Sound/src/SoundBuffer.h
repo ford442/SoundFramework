7617914b611d6c215bd1e2c72b3581b8de6435f1
Noah Cohn <noahc42@gmail.com>
2023-03-21 13:29:22 -0700

    Update SoundBuffer.h

--- Sound/src/SoundBuffer.h
+++ Sound/src/SoundBuffer.h
@@ -6,7 +6,7 @@
 #include <AL/alc.h>
 #include "/content/RAMDRIVE2/SoundFramework/Sound/libsndfile/include/sndfile.hh"
 #include <inttypes.h>
-#include <AL/alext.h>
+#include </content/RAMDRIVE2/SoundFramework/Sound/openal-soft/include/AL/alext.h>
 
 void DisplayALError(const char* msg,ALenum error);
 void DisplayALCError(const char* msg,ALCenum error);
