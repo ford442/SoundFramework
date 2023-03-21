47c9d93ee2987fa0f365c58a5c32eb6cb81a4658
Noah Cohn <noahc42@gmail.com>
2023-03-21 12:46:41 -0700

    Update SoundDevice.cpp

--- Sound/src/SoundDevice.cpp
+++ Sound/src/SoundDevice.cpp
@@ -1,39 +1,16 @@
 #include "SoundDevice.h"
 
-SoundDevice::SoundDevice()
-{
-   // open default device
-   m_Device = alcOpenDevice(nullptr);
- //  if (!m_Device) // verify success
-
-   // create new context
-   m_Context = alcCreateContext(m_Device, nullptr);
- //  if (!m_Context)   // verify success
-
-   // make context current
-  alcMakeContextCurrent(m_Context);   const ALCchar* name = nullptr;
-/* 
-   if (alcIsExtensionPresent(m_Device, "ALC_ENUMERATE_ALL_EXT"))
-      name = alcGetString(m_Device, ALC_ALL_DEVICES_SPECIFIER);
-   else
-
-   if (!name || !alcGetError(m_Device))
-      name = alcGetString(m_Device, ALC_DEVICE_SPECIFIER);
-
- std::cout << "Opened Device: " << name << std::endl; 
- */
+SoundDevice::SoundDevice(){
+m_Device=alcOpenDevice(nullptr);
+m_Context=alcCreateContext(m_Device, nullptr);
+alcMakeContextCurrent(m_Context);
+const ALCchar* name=nullptr;
 }
 
-SoundDevice::~SoundDevice()
-{
-   // clear current context
-   if (!alcMakeContextCurrent(nullptr))
-   DisplayALCError("Error Unbinding Current Context", alcGetError(m_Device));
-
-   // destroy context
-   alcDestroyContext(m_Context);
-   DisplayALCError("Error Destroying Context", alcGetError(m_Device));
-
-   // close active device
-   alcCloseDevice(m_Device);
+SoundDevice::~SoundDevice(){
+if(!alcMakeContextCurrent(nullptr))
+DisplayALCError("Error Unbinding Current Context",alcGetError(m_Device));
+alcDestroyContext(m_Context);
+DisplayALCError("Error Destroying Context",alcGetError(m_Device));
+alcCloseDevice(m_Device);
 }
