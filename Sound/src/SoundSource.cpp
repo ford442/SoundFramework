01578b76898ed0311fca48b8c96321540b7d097e
Noah Cohn <noahc42@gmail.com>
2023-03-21 12:45:09 -0700

    Update SoundSource.cpp

--- Sound/src/SoundSource.cpp
+++ Sound/src/SoundSource.cpp
@@ -1,29 +1,27 @@
 #include "SoundSource.h"
 
 SoundSource::SoundSource(){
-alGenSources(1, &m_Source);
-alSourcef(m_Source, AL_PITCH, m_Pitch);
-alSourcef(m_Source, AL_GAIN, m_Gain);
-alSource3f(m_Source, AL_POSITION, m_Position[0], m_Position[1], m_Position[2]);
-alSource3f(m_Source, AL_VELOCITY, m_Velocity[0], m_Velocity[1], m_Velocity[2]);
-alSourcei(m_Source, AL_LOOPING, m_Loop);
-alSourcei(m_Source, AL_BUFFER, 0);
+alGenSources(1,&m_Source);
+alSourcef(m_Source,AL_PITCH,m_Pitch);
+alSourcef(m_Source,AL_GAIN,m_Gain);
+alSource3f(m_Source,AL_POSITION,m_Position[0],m_Position[1],m_Position[2]);
+alSource3f(m_Source,AL_VELOCITY,m_Velocity[0],m_Velocity[1],m_Velocity[2]);
+alSourcei(m_Source,AL_LOOPING,m_Loop);
+alSourcei(m_Source,AL_BUFFER, 0);
 DisplayALError("Error Creating Sound Source", alGetError());
 }
 
 SoundSource::~SoundSource(){
-// unassign buffer ID from source
-alSourcei(m_Source, AL_BUFFER, 0);
-// delete source
-alDeleteSources(1, &m_Source);
+alSourcei(m_Source,AL_BUFFER,0);
+alDeleteSources(1,&m_Source);
 DisplayALError("Error Destroying Sound Source", alGetError());
 }
 
 void SoundSource::Play(SoundBuffer* buffer){
 if(buffer){
-if(buffer != m_Buffer){
-m_Buffer = buffer;
-alSourcei(m_Source, AL_BUFFER, (ALint)m_Buffer->GetID());
+if(buffer!=m_Buffer){
+m_Buffer=buffer;
+alSourcei(m_Source,AL_BUFFER,(ALint)m_Buffer->GetID());
 }
 alSourcePlay(m_Source);
 }else{
