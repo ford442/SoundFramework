3bfa3f21a68136b09ab1dc6727f39cecd4fb0d64
Noah Cohn <noahc42@gmail.com>
2023-03-21 12:47:59 -0700

    Update SoundBuffer.cpp

--- Sound/src/SoundBuffer.cpp
+++ Sound/src/SoundBuffer.cpp
@@ -1,26 +1,20 @@
 #include "SoundBuffer.h"
 
 SoundBuffer::SoundBuffer(const char* audioFilename){
-   // adapted from OpenAL example alplay.c
-   // https://github.com/kcat/openal-soft/blob/master/examples/alplay.c
 ALenum err,format;
 SNDFILE* soundEffect;
 SF_INFO sfinfo;
 short* membuf;
 sf_count_t num_frames;
 ALsizei num_bytes;
-   // open audio file and check that it is usable
 soundEffect = sf_open(audioFilename, SFM_READ, &sfinfo);
 if (!soundEffect){
 std::cout << "Could not open audio file: " << audioFilename << std::endl;
 }
-   // verify file has valid data
 if (sfinfo.frames < 1 || sfinfo.frames >(sf_count_t)(INT_MAX / sizeof(short)) / sfinfo.channels){
 std::cout << "Audio file: " << audioFilename << " : produced invalid data" << std::endl;
 sf_close(soundEffect);
 }
-
-// get sound format and configure openal format
 format = AL_NONE;
 if (sfinfo.channels == 1)format = AL_FORMAT_MONO16;
 else if (sfinfo.channels == 2)format = AL_FORMAT_STEREO16;
@@ -35,8 +29,6 @@ if (!format){
 std::cout << "Unsuported channel count/format: " << audioFilename << std::endl;
 sf_close(soundEffect);
 }
-   // decode the audio file into a buffer
-   // static cast required since c++ yells at it
 membuf = static_cast<short*>(malloc((size_t)(sfinfo.frames * sfinfo.channels) * sizeof(short)));
 num_frames = sf_readf_short(soundEffect, membuf, sfinfo.frames);
 if (num_frames < 1){
@@ -45,14 +37,10 @@ sf_close(soundEffect);
 std::cout << "Failed to read samples in audio file: " << audioFilename << std::endl;
 }
 num_bytes = (ALsizei)(num_frames * sfinfo.channels) * (ALsizei)sizeof(short);
-   //! OpenAL stuff
-   // create and fill this data into an OpenAL buffer
 alGenBuffers(1, &m_ID);
 alBufferData(m_ID, format, membuf, num_bytes, sfinfo.samplerate);
-   // clean up audio file now that we are done with it
 free(membuf);
 sf_close(soundEffect);
-   // check for AL errors
 while (err = alGetError()){
 //  DisplayALError("AL Error in " __FUNCTION__ ": ", err);  // throws error __FUNCTION__ non-existant
 alDeleteBuffers(1, &m_ID);
@@ -61,9 +49,7 @@ std::cout << "Loaded audio file :'" << audioFilename << "' with ID (" << m_ID <<
 }
 
 SoundBuffer::~SoundBuffer(){
-   // delete the buffer from AL memory
 alDeleteBuffers(1, &m_ID);
-   // check for error
 DisplayALError("Error Destroying Sound Buffer", alGetError());
 }
 
